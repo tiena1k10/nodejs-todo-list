8c2f746a869ba1640ff33d39474c95402e8ec692
tiena1k10 <tiena1k10@gmail.com>
2022-05-28 20:54:45 +0700

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 // setup public folder
 app.use(express.static("./src/public"));
 app.use(express.json());
+require('dotenv').config()
 const port = process.env.PORT || 8888
 const db = require("./src/db/connect");
 const notFound = require('./src/middleware/not-found');
@@ -14,7 +15,6 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
-require('dotenv').config()
 
 db.connect().then(()=>{
     console.log("Connect Database thanh cong");
